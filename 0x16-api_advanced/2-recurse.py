f631a69c3755034eb09bc28c8f72196d4823f984
stuartses <stuart.ses@hotmail.com>
2020-05-07 17:39:00 -0500

    Change returns

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -31,6 +31,6 @@ def recurse(subreddit, hot_list=[], after=""):
         hot_list.append(post.get('data').get('title'))
 
     if after is not None:
-        hot_list += recurse(subreddit, hot_list, after)
-
-    return hot_list
+        return hot_list + recurse(subreddit, hot_list, after)
+    else:
+        return []
